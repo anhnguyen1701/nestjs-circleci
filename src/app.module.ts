7055e081008250438ba80df4d135cde9e991fc20
anh nguyen <nguyenlamanh1701@gmail.com>
2022-04-26 10:20:42 +0700

    config mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'test',
+      entities: [join(__dirname, '**', '*.entity.{ts, js}')],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
